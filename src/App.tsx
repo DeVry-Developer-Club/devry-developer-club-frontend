015823ce265d6abf8f49ef4458d896591483a5d0
Michael <michael.desormeaux@scswla.org>
2021-08-17 07:59:02 -0500

    feat: added support for TinyMCE in the future

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import {GlobalStyle} from "./AppStyle";
-import {Layout} from "./components/Layout/Layout";
-import {Route} from "react-router-dom";
+import { GlobalStyle } from "./AppStyle";
+import { Layout } from "./components/Layout/Layout";
+import { Route } from "react-router-dom";
 
 export const App = () => {
-    return (
-        <>
-            <GlobalStyle/>
-            <Layout>
-                <Route exact path="/">Testing</Route>
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Route exact path="/">
+          Testing
+        </Route>
+      </Layout>
+    </>
+  );
 };
